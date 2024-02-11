e32a5c0d596895e7988207c1102c6764bf5a2f60
leejeawon92 <jeawon33333@gmail.com>
2024-02-11 23:15:54 +0900

    nestJS: controller, module, service description

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,21 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello(); // 이 코드는 왜 필요할까? 이 해답은 구조와 아키텍처에 대해 이해해야 한다
+  }
+  @Get('/welcome')  
+  saywelcome(): string { // express에서 controller와 같은 역할 - http://localhost:3000/welcome으로 접속하면 return부분이 보여질 것이다. 
+    return '환영합니다.';
+  }
+  @Post('/post')
+  saypost(): string {
+    return '요청';
   }
 }
+
+//1. app.module.ts에서 @Module()데코레이터 안에 [AppController]는 위 코드와 같다.
+//2. @Controller()안에 @Get()이라는 데코레이터가 있고, string을 return하는 getHello함수가 있다.
+//3. this.appService.getHello();에서 appService는 생성자(constructor)에서 오는 것을 볼 수 있다.
+//4. AppController가 하는 일은 기본적으로 url을 가져오고 함수를 실행하는 것이다.  
+//   예를들어 express에서 controller/router 같은 존재이다. src/app.controller.ts파일에서 Get데코레이터가 express의 get라우터와 같은 역할을 한다
+//5. @Get, @Post을 작성하는 것만으로도 get request, post request를 할 수 있게 되었다
--- src/app.module.ts
+++ src/app.module.ts
@@ -8,3 +8,10 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+
+//1. class AppModule {}와 같이 클래스는 비어있지만, 진짜는 @Module()안에 있다. 
+//2. @Module는 데코레이터라고 한다. 데코레이터는 클래스에 함수 기능을 추가할 수 있다. nestJS에서 자주사용된다.
+//   즉, 클래스 위의 함수이고 클래스를 위해 움직인다고 생각하면 된다.
+//3. const app = await NestFactory.create(AppModule);와 같이 하나의 모듈에서 애플리케이션을 생성한다.
+//4. 모듈은 애플리케이션의 일부분이다. 예를들어 인증을담당한다건가, 사진을업로드한다던가 하나의 기능을 한다
+//   AppModule은 루트모듈 같은 것이다. 
